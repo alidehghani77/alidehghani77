b062ff1558cda31a7cf25d327d8e964fc5729089
alidehghani77 <ali.dehghani@gmail.com>
2022-04-20 23:29:31 +0430

    change test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,9 @@
-import numpy as py
-arr = py.array([1,2,3,4])
-for x in arr:
-    print(x)
+# import numpy as py
+# arr = py.array([1,2,3,4])
+# for x in arr:
+#     print(x)
+    
+a = [10,20]
+b = a
+b = [30,40]
+print(a)
